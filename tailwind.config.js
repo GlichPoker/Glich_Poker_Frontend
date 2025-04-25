2d422b865ee6ebf27dbf5095c876ac4ff44770d0
RealBlueSwan <nobrunner@hotmail.com>
2025-04-25 12:41:19 +0200

    zinc color pallet

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,18 @@ module.exports = {
     extend: {
       colors: {
         // You can define custom colors here if needed
+        zinc: {
+          900: '#18181b',
+          800: '#27272a',
+          700: '#3f3f46',
+          600: '#52525b',
+          500: '#71717a',
+          400: '#a1a1aa',
+          300: '#d4d4d8',
+          200: '#e4e4e7',
+          100: '#f4f4f5',
+          50: '#fafafa',
+        },
       },
     },
   },
